2ff9c06f30d09ea246e87446539d6728ab7eccac
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-05 09:36:24 +0800

    Add Users Service

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -1,38 +1,17 @@
-
-import { of as observableOf,  Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
 
-let counter = 0;
+  constructor(private http: HttpClient) { }
 
-@Injectable()
-export class UserService {
+  baseUri = "http://localhost:4000";
 
-  private users = {
-    nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
-    eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
-    jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
-    lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
-    alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png' },
-    kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png' },
-  };
-
-  private userArray: any[];
-
-  constructor() {
-    // this.userArray = Object.values(this.users);
+  getUsers() {
+    return this.http.get(`${this.baseUri}/users`);
   }
 
-  getUsers(): Observable<any> {
-    return observableOf(this.users);
-  }
-
-  getUserArray(): Observable<any[]> {
-    return observableOf(this.userArray);
-  }
-
-  getUser(): Observable<any> {
-    counter = (counter + 1) % this.userArray.length;
-    return observableOf(this.userArray[counter]);
-  }
 }
