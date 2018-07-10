2ef7c77adc416c2f07b0b9564fda6a845935daee
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-10 15:32:22 +0800

    Update Header add logout routing

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -4,6 +4,9 @@ import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/usersold.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
 
+// import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { NbAuthJWTToken, NbAuthService } from '../../components/auth';
+
 @Component({
   selector: 'ngx-header',
   styleUrls: ['./header.component.scss'],
@@ -14,19 +17,31 @@ export class HeaderComponent implements OnInit {
 
   @Input() position = 'normal';
 
-  user: any;
+  // user: any;
+  user = {};
 
-  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+  userMenu = [{ title: 'Profile' }, { title: 'Log out', link: '/auth/logout' }];
 
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
-              private analyticsService: AnalyticsService) {
+              private analyticsService: AnalyticsService,
+              private authService: NbAuthService) {
+
+    this.authService.onTokenChange()
+        .subscribe((token: NbAuthJWTToken) => {
+        
+        if (token.isValid()) {
+          this.user = token.getPayload(); 
+          // console.log(this.authService.getToken());
+        } 
+    });
+
   }
 
   ngOnInit() {
-    this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.nick);
+    // this.userService.getUsers()
+    //   .subscribe((users: any) => this.user = users.nick);
   }
 
   toggleSidebar(): boolean {
