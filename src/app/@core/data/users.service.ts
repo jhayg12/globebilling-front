764d113a793793b68bd83e9dd64144f4d42fb577
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-18 15:21:33 +0800

    Update Users Service

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -14,5 +14,35 @@ export class UsersService {
     return this.http.get(`${this.baseUri}/users`);
   }
 
+  addUser(userInfo) {
+    const user = {
+      username: userInfo.username,
+      fullName: userInfo.fullname,
+      email: userInfo.email,
+      password: userInfo.password,
+      role: userInfo.role
+    }
+
+    return this.http.post(`${this.baseUri}/users/add`, user);
+
+  }
+  
+  updateUser(id, username, fullname, email, password, role) {
+    const user = {
+      username: username,
+      fullname: fullname,
+      email: email,
+      password: password,
+      role: role
+    }
+
+    return this.http.post(`${this.baseUri}/users/update/${id}`, user);
+
+  }
+
+  deleteUser(id) {
+    return this.http.get(`${this.baseUri}/users/delete/${id}`);
+  }
+
 
 }
