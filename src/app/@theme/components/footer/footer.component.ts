091f68b59745140dae22128dbfddcf9654d841d4
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-19 16:02:51 +0800

    Update Footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,12 +5,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">Created by <b><a href="https://www.iscale-solutions.com/" target="_blank">IScale Solutions Inc.</a></b> 2017</span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
   `,
 })
 export class FooterComponent {
